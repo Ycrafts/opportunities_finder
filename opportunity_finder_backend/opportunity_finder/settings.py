219e17849309a6fe9b1ba7c6b243578bbb1042c1
yonatanassefa <yonatanassefa60@gmail.com>
2026-02-03 19:55:43 +0300

    Add whitenoise to serve static files

--- opportunity_finder_backend/opportunity_finder/settings.py
+++ opportunity_finder_backend/opportunity_finder/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -373,7 +374,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media (uploads) - used for CV file reference storage in `profiles`
 MEDIA_URL = "/media/"
